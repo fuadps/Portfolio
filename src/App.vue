f6fd3906830c558a1a17b49aa0b3def83708e0a5
Fuad <fuadpls@gmail.com>
2022-08-04 13:37:46 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterView } from "vue-router";
+import Header from "@/components/Head.vue";
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+</template>
---
+++ src/components/Head.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="border-t border-blue-600">
+    <nav>this is header</nav>
+  </header>
+</template>
